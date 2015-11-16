a65ac6fa556965643aa4548cb74dc0e7bfd07741
ddeepak6992 <ddeepak6992@gmail.com>
2015-11-16 22:20:17 +0530

    Update BTree.py

--- Binary-Tree/BTree.py
+++ Binary-Tree/BTree.py
@@ -6,6 +6,7 @@ Created on Wed Nov 11 13:24:31 2015
 """
 
 import random
+import Queue
 
 class BTree():
     def __init__(self):
@@ -79,6 +80,64 @@ def root_to_leaf_cover(root):
             print n,
         print ''
 
+#count of single valued subtreses
+def SVSubTrees(root):
+    count1,count2,st1,st2 = 0,0,True,True
+    if root.left:
+        count1,st1 = SVSubTrees(root.left)
+    if root.right:
+        count2,st2 = SVSubTrees(root.right)
+    if root.left is None and root.right is None:
+        return 1,True
+    if root.left and root.right:
+        if root.value == root.left.value and root.value == root.right.value:
+            if st1 is True and st2 is True:
+                st = True
+                count = count1+count2 + 1
+            else:
+                count = count1+count2
+                st = False
+        else:
+            count = count1+count2
+            st = False
+    else:
+        if root.left:
+            if root.value == root.left.value:
+                count  = count1+1
+                st = True
+            else:
+                count = count1
+                st = False
+        else:
+            if root.value == root.right.value:
+                count  = count1+1
+                st = True
+            else:
+                count = count1
+                st = False
+    return count,st
+                
+def maxPathSum(root):
+    pass
+    
+def levelOrder(root):
+    q = Queue.Queue()
+    q.put(root)
+    while not q.empty():
+        x = q.get()
+        print x.value
+        if x.left is not None:
+            q.put(x.left)
+        if x.right is not None:
+            q.put(x.right)
+            
+def levelOrderTraversalReverse():
+    pass
+def levelOrderTraversalSpiral():
+    pass
+        
+
+
 
 root1 = BTree()
 root1.value = 0
@@ -86,6 +145,6 @@ generateRandomTree(root1,2)
 inorder(root1)
 
 print ''
-
+print levelOrder(root1)
 
 
